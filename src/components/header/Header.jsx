3a7498de56f09eddabe2f1da2ef3a94ec0f98b50
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-01-22 18:52:39 +0100

    Ajoute fichier de base

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="" alt="logo" />
+      <nav className="navigation">
+        <a href="">Home</a>
+        <a href="">À propos</a>
+        <a href="">Compétances</a>
+        <a href="">Projets</a>
+      </nav>
+      <a href="">Contact</a>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/header/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <div>Bonjour</div>
+    </div>
+  );
+}
+
+export default Home;
