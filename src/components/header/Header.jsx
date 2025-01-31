4c3bdd68d5b09c29435efd82cacfa41a9327f6f6
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-01-31 15:55:08 +0100

    Ajout Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,19 @@
 import "./header.css";
+import logo from "../../assets/logo-nathan-rodriguez.png";
 
 function Header() {
   return (
     <header className="header">
-      <img src="" alt="logo" />
+      <img className="logo-header" src={logo} alt="logo" />
       <nav className="navigation">
         <a href="">Home</a>
         <a href="">À propos</a>
         <a href="">Compétances</a>
         <a href="">Projets</a>
       </nav>
-      <a href="">Contact</a>
+      <a href="" className="btn-contact">
+        Contactez-moi
+      </a>
     </header>
   );
 }
