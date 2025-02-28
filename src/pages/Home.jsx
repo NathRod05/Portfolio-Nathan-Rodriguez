a202c33738d1f8f82e17b7e4cb7fc05912ef180e
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-02-28 22:09:36 +0100

    Page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
+import Form from "../components/Modal-form/form";
+import "./home.css";
+import About from "../components/about/About";
+import Skill from "../components/skill/Skill";
 
 function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div>
-      <Header />
-      <div>Bonjour</div>
-      <Footer />
+    <div className="home">
+      <Header onOpen={handleOpenModal} />
+      {isModalOpen && (
+        <div className="overlay">
+          <div className="modal">
+            <Form onClose={handleCloseModal} />
+          </div>
+        </div>
+      )}
+      <About />
+      <Skill />
+      <Footer onOpen={handleOpenModal} />
     </div>
   );
 }
