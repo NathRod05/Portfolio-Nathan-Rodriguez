120f32111a1542158bde5134e1ffa98a58bb21b4
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-03-01 03:34:14 +0100

    Ajout CV

---
+++ src/components/cv/cv.jsx
@@ -0,0 +1,21 @@
+import "./cv.css";
+import imgCv from "../../assets/CV-Portfolio.jpg";
+import cvPdf from "../../assets/CV-Portfolio.pdf";
+
+function CV() {
+  return (
+    <div className="cv-container" id="cv">
+      <h1>CV</h1>
+      <div>
+        <img className="image-cv" src={imgCv} alt="CV de Nathan Rodriguez" />
+      </div>
+      <div>
+        <a href={cvPdf} download="Mon_CV.pdf">
+          <button className="btn-download">Télécharger mon CV</button>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default CV;
