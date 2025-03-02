4e32390c853f1ad528589b8da0e67b4a6a623d4e
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-03-02 06:25:49 +0100

    Ajout des fichier card

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,17 @@
+import "./card.css";
+
+function Card({ data }) {
+  return (
+    <div className="card" id={data.id}>
+      <a href={data.link} target="blank">
+        <img className="image-card" src={data.image} alt={data.attribut} />
+        <div className="text">
+          <h2>{data.titre}</h2>
+          <p>{data.description}</p>
+        </div>
+      </a>
+    </div>
+  );
+}
+
+export default Card;
