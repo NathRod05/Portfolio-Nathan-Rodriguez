9dd7d60d9fb5e9134eed324d726a19a8f6287e90
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-03-03 19:15:15 +0100

    Ajout librairie

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Home from "./pages/Home";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return <Home />;
+  return;
+  <div>
+    <Home />;
+    <Analytics />
+  </div>;
 }
 
 export default App;
