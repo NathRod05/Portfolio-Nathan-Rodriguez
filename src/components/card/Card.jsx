158905c0e933b3cc9d02d64b327337941d8c7335
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-03-06 02:00:40 +0100

    Ajout _blank

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,7 +3,7 @@ import "./card.css";
 function Card({ data }) {
   return (
     <div className="card" id={data.id}>
-      <a href={data.link} target="blank">
+      <a href={data.link} target="_blank">
         <img className="image-card" src={data.image} alt={data.attribut} />
         <div className="text">
           <h2>{data.titre}</h2>
