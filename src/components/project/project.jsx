f6d7c872d4ee38659e92c7def0c5963777d570a9
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-03-12 22:22:45 +0100

    Ajout des cards en anglais

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -5,11 +5,12 @@ import { useTranslation } from "react-i18next";
 
 function Project() {
   const { t, i18n } = useTranslation();
+
   return (
     <div className="project-container" id="project">
       <h1>{t("project")}</h1>
       <div className="cards-container">
-        {data.website.map((card) => (
+        {data[i18n.language].map((card) => (
           <Card key={card.id} data={card} />
         ))}
       </div>
