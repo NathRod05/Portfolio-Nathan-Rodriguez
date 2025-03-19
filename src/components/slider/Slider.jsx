b960e310215ba73393603a8446bcd261d8a454b0
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-03-19 15:42:02 +0100

    Suppression des import slider pour un data.slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,19 +1,14 @@
 import { useState, useEffect } from "react";
-import slider1 from "../../assets/slider-1.jpg";
-import slider2 from "../../assets/slider-2.jpg";
-import slider3 from "../../assets/slider-3.jpg";
-import slider4 from "../../assets/slider-4.png";
-import slider5 from "../../assets/slider-5.jpg";
+import sliderData from "../../../data.json";
 import "./slider.css";
 
 function Slider() {
-  const slider = [slider1, slider2, slider3, slider4, slider5];
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
     const nextCard = () => {
       setIndex((prevIndex) =>
-        prevIndex < slider.length - 1 ? prevIndex + 1 : 0
+        prevIndex < sliderData.slider.length - 1 ? prevIndex + 1 : 0
       );
     };
     const interval = setInterval(nextCard, 4000);
@@ -22,10 +17,14 @@ function Slider() {
   return (
     <div className="slider-container">
       <div className="slider">
-        <img className="img" src={slider[index]} alt={`Slide ${index + 1}`} />
+        <img
+          className="img"
+          src={sliderData.slider[index].image}
+          alt={`Slide ${index + 1}`}
+        />
       </div>
       <div className="dots">
-        {slider.map((_, radioIdx) => (
+        {sliderData.slider.map((_, radioIdx) => (
           <input
             key={radioIdx}
             type="radio"
