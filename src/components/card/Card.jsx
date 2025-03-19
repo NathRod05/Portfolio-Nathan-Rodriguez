86a777918c13c3fbace827d979236c0b095671de
Nathan Rodriguez <nathan94_roro@hotmail.fr>
2025-03-19 15:41:10 +0100

    Ajout d'un mapping skills

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,16 +5,15 @@ function Card({ data, icons }) {
   return (
     <div className="card" id={data.id}>
       <a href={data.link} target="_blank">
-        <img className="image-card" src={data.image} alt={data.attribut} />
+        <img className="image-card" src={data.image} alt={data.alt} />
         <div className="text">
           <h2>{data.titre}</h2>
           <p>{data.description}</p>
           <div className="skill-card">
-            {data.html && <p>{data.html}</p>}
-            {data.css && <p>{data.css}</p>}
-            {data.sass && <p>{data.sass}</p>}
-            {data.react && <p>{data.react}</p>}
-            {data.javascript && <p>{data.javascript}</p>}
+            {data.skills &&
+              data.skills.map((skillObj, index) => (
+                <p key={index}>{skillObj.skill}</p>
+              ))}
           </div>
         </div>
       </a>
